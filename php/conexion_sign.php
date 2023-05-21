23ae4c951f9bc1b9555ee198b252f0dc2912300b
Uriel de Jesús Ruvalcaba Becerra <uriel.ruvalcaba9185@alumnos.udg.mx>
2023-05-20 23:05:37 -0600

    20/05

--- php/conexion_sign.php
+++ php/conexion_sign.php
@@ -1,9 +1,7 @@
 <?php
-<<<<<<< HEAD
+@include 'config.php';
+
 $conexion = mysqli_connect("localhost", "root", "", "perfiles");
-=======
-$conexion = mysqli_connect("localhost", "root", "12345", "ecommerce");
->>>>>>> 965ba8ecb8f07144b0d4766a5e2bc558f45df082
 
 /*
 if($conexion){
