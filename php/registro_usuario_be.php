f87b0f23f5730f437b8ac5904aa013134ef49c31
Cristofer Hernandez <70930943+crishdfranco@users.noreply.github.com>
2023-05-21 15:18:37 -0600

    Correccion de la variable nombre, no ingresaba nada a la base de datos

--- php/registro_usuario_be.php
+++ php/registro_usuario_be.php
@@ -7,40 +7,35 @@ $correo = $_POST['correo'];
 $contrasena = $_POST['contrasena'];
 $contrasena = hash('sha12', $contrasena);
 
-$query = "INSERT INTO personas(Nombres, correo, pasword) 
-VALUES('$nombre_completo', '$correo', '$contrasena')"; 
- 
-//verificar que no se repitan el correo
-$verificar_correo = mysqli_query($conexion, "SELECT * FROM personas WHERE correo='$correo' ");
-
-$query = "INSERT INTO personas(Nombres, correo, pasword) 
-VALUES('$Nombres', '$correo', '$contrasena')"; 
- 
-//verificar que no se repitan el correo
+// Verificar que no se repita el correo
 $verificar_correo = mysqli_query($conexion, "SELECT * FROM personas WHERE correo='$correo'");
 
-if(mysqli_num_rows($verificar_correo) > 0){
-    echo'
-    <Script>
+if (mysqli_num_rows($verificar_correo) > 0) {
+    echo '
+    <script>
     alert("El correo ya se ha registrado");
     window.location = "../sign_up.php";
     </script>
     ';
     exit();
+} else { // Correo único - se verificó ya
+    $query = "INSERT INTO personas(Nombres, correo, pasword) 
+    VALUES('$Nombres', '$correo', '$contrasena')"; 
+    $ejecutar = mysqli_query($conexion, $query);
 }
 
-$ejecutar = mysqli_query($conexion, $query);
-
-if($ejecutar){
-    echo'<script>
+if ($ejecutar) {
+    echo '<script>
     alert("Registro exitoso");
     window.location = "../sign_up.php";
     </script>';
-}else{
-    echo'<script>
-    alert("No se pudo registrar, inetentalo mas tarde");
+} else {
+    echo '<script>
+    alert("No se pudo registrar, inténtalo más tarde");
     window.location = "../sign_up.php";
     </script>';
 }
-    mysqli_close($conexion);
+
+mysqli_close($conexion);
+
 ?>
