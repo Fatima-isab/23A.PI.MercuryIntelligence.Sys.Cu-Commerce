dce55f87d819dad64a982162f54adf3eec67e9b4
Cristofer Hernandez <70930943+crishdfranco@users.noreply.github.com>
2023-05-21 20:40:38 -0600

    Funcion para comprar

---
+++ php/compras.php
@@ -0,0 +1,50 @@
+<?php
+function agregar_folio($IdProductos, $IdClientes) {
+  // Establecer la conexión con la base de datos
+  $servername = "localhost";
+  $username = "tu_usuario";
+  $password = "tu_contraseña";
+  $dbname = "tu_basededatos";
+
+  // Crear la conexión
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
+  // Verificar si la conexión es exitosa
+  if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+  }
+
+  $IdVendedor = 0;
+  $mensaje = "";
+
+  // Recuperar el ID del vendedor desde la tabla "productos"
+  $sql = "SELECT IdVendedor FROM productos WHERE IdProductos = $IdProductos";
+  $result = $conn->query($sql);
+
+  if ($result->num_rows > 0) {
+    // Obtener el resultado de la consulta
+    $row = $result->fetch_assoc();
+    $IdVendedor = $row["IdVendedor"];
+  }
+
+  // Insertar el nuevo folio en la tabla "folios"
+  $sql = "INSERT INTO folios (IdProducto, IdCliente, IdVendedor) VALUES ($IdProductos, $IdClientes, $IdVendedor)";
+  $conn->query($sql);
+
+  // Obtener el ID generado para el nuevo folio
+  $folioId = $conn->insert_id;
+
+  // Llamar a la función "actualizar_mensaje" y obtener el mensaje actualizado
+  $mensaje = modificar_mensaje($folioId);
+
+  // Actualizar el mensaje en la tabla "folios"
+  $sql = "UPDATE folios SET Mensaje = CONCAT('¡Querido usuario, se ha generado un encargo con éxito! ', '$mensaje') WHERE IdFolios = $folioId";
+  $conn->query($sql);
+
+  // Cerrar la conexión
+  $conn->close();
+
+  // Devolver el ID del folio generado
+  return $folioId;
+}
+?>
--- producto.php
+++ producto.php
@@ -1,8 +1,9 @@
 <?php 
+
 // Configuración de la conexión a la base de datos
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "root";
 $dbname = "nueva_cucomerce";
 
 // Crear conexión
@@ -39,6 +40,55 @@ if ($producto) {
     exit();
 }
 
+// Compra 
+function agregar_folio($IdProductos, $IdClientes) {
+    // Establecer la conexión con la base de datos
+    $servername = "localhost";
+    $username = "root";
+    $password = "root";
+    $dbname = "nueva_cucomerce";
+  
+    // Crear la conexión
+    $conex = new mysqli($servername, $username, $password, $dbname);
+  
+    // Verificar si la conexión es exitosa
+    if ($conex->connect_error) {
+      die("Error de conexión: " . $conex->connect_error);
+    }
+  
+    $IdVendedor = 0;
+    $mensaje = "";
+  
+    // Recuperar el ID del vendedor desde la tabla "productos"
+    $sql = "SELECT IdVendedor FROM productos WHERE IdProductos = $IdProductos";
+    $result = $conex->query($sql);
+  
+    if ($result->num_rows > 0) {
+      // Obtener el resultado de la consulta
+      $row = $result->fetch_assoc();
+      $IdVendedor = $row["IdVendedor"];
+    }
+  
+    // Insertar el nuevo folio en la tabla "folios"
+    $sql = "INSERT INTO folios (IdProducto, IdCliente, IdVendedor) VALUES ($IdProductos, $IdClientes, $IdVendedor)";
+    $conex->query($sql);
+  
+    // Obtener el ID generado para el nuevo folio
+    $folioId = $conex->insert_id;
+  
+    // Llamar a la función "actualizar_mensaje" y obtener el mensaje actualizado
+    $mensaje = modificar_mensaje($folioId);
+  
+    // Actualizar el mensaje en la tabla "folios"
+    $sql = "UPDATE folios SET Mensaje = CONCAT('¡Querido usuario, se ha generado un encargo con éxito! ', '$mensaje') WHERE IdFolios = $folioId";
+    $conex->query($sql);
+  
+    // Cerrar la conexión
+    $conex->close();
+  
+    // Devolver el ID del folio generado
+    return $folioId;
+  }
 ?>
 
 <!DOCTYPE html>
