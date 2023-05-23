ef8c862467dd1379e8f390215b3ab794effcf322
Fatima-isab <fatima.roman2953@alumnos.udg.mx>
2023-05-22 21:33:44 -0600

    22/05

--- administrador.php
+++ administrador.php
@@ -1,10 +1,31 @@
 <?php
+include 'conexion_sign.php';
 
+$correo = $_POST['correo'];
+$contrasena = $_POST['contrasena'];
+$contrasena = hash('sha512', $contrasena);
 session_start();
-if(isset($_SESSION['usuario']) && isset($_SESSION['administrador']) && $_SESSION['administrador'] == true) {
- echo "usuario es un administrador";
-} else {
-  echo "usuario no es un administrador";
-}
+$_SESSION['usuario']=$correo;
 
-?>
+
+$consulta="SELECT*FROM personas WHERE correo='$correo' 
+and contrasena ='$contrasena'";
+$resuladmi=mysqli_query($conexion, $consulta);
+$filas=mysqli_fetch_array($resuladmi);
+
+if($filas['rol']==1){
+    header("location: admin.php");
+}else if($filas['rol']==2){
+        header("location: inicio.php");
+    
+}
+else{
+    ?>
+    <?php
+    include("inicio.php");
+    ?>
+    <h1 class="bad">Error en la autenticacion</h1>
+    <?php
+}
+mysqli_free_result($resuladmi);
+mysqli_close($conexion);
