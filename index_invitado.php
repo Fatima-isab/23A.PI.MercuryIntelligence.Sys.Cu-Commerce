4c83cc1d95b04b6adb3c1f8f9582ce64c89f491c
UR13G13Z <112902642+UR13G13Z@users.noreply.github.com>
2023-05-22 21:58:00 -0600

    invitado

--- index_invitado.php
+++ index_invitado.php
@@ -39,13 +39,18 @@ $result = $conn->query($sql);
     <title>Cu-Commerce</title>
     <link rel="shortcut icon" href="../assets/img/Logo.png">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+
+    <script src="https://kit.fontawesome.com/03a89292db.js" crossorigin="anonymous"></script>
 
     <!-- Para bootstrap-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 
-<body>
+<body class="dark">
+             
+
     <header>
     <div id="CtnHead" class="row shadow">
             <!-- aqui va el logo de la plataforma -->
@@ -80,6 +85,10 @@ $result = $conn->query($sql);
                 </li>
                 
             </ul>
+            <br>
+       <div class="modo" id="modo" >
+                          <i class="fas fa-toggle-on"></i>
+                    </div>
                 <div id="modal" class="modal">
                     <div class="modal-content" >
                         <span class="cerrar">&times;</span>
@@ -176,6 +185,7 @@ $result = $conn->query($sql);
         crossorigin="anonymous"></script>
 
     <script src="assets/scripts/inicio.js"></script>
+    <script src="assets/scripts/dark.js"></script>
 
 </body>
 </html>
